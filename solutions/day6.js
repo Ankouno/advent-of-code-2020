57f8b2afcf3c9f04d19451fd365b8a0a609b52b3
kaizoman <kaizoman666@gmail.com>
2020-12-06 04:02:25 -0500

    Fixing day 6

--- solutions/day6.js
+++ solutions/day6.js
@@ -3,14 +3,14 @@ const { uniq } = require('lodash');
 const _ = require('lodash');
 
 const input = fs.readFileSync('inputs/day6.txt');
-const groups = _.compact(_.split(input, '\n\n')).map((group) => group.split("\n"));
+const groups = _.split(input, '\n\n').map((group) => _.compact(group.split("\n")));
 
 const firstSolution = () => {
   return groups.reduce((a, group) => a + _.uniq(group.join("")).length, 0)
 }
 
 const secondSolution = () => {
-  return groups.reduce((a, group) => a + _.intersection(...group.map(a => a.split(''))).length, 0)
+  return groups.reduce((a, group) => a + _.intersection(...group.map(p => p.split(''))).length, 0)
 }
 
 console.log("==[Day 6]=========")
